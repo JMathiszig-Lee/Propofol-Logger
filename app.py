67175fca78d3eb22fd7847fd0cd7b503adea8c32
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-22 22:28:23 +0800

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from connexion.resolver import RestyResolver
-import connexion
-
-
-if __name__ == '__main__':
-    app = connexion.FlaskApp(__name__, specification_dir='swagger/')
-    app.add_api('api.yaml', resolver=RestyResolver('api'))
-    app.run(host='127.0.0.1', port=9090)
